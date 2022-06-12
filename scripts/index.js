3404dd8a4909d1aeea224785ebe0bb18c2cba0e6
Mariya Stison <ladykleo@yandex.ru>
2022-06-12 21:37:51 +0300

    fix: Исправить замечания по index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,7 @@
 //Находим поп-ап, кнопку Редактирования профиля, кнопку закрытия поп-апа
 let popup = document.querySelector('.popup');
-let addBtn = document.querySelector('.btn_type_edit');
-let closeBtn = document.querySelector('.btn_type_close');
-
-//Определяем функцию открытия поп-апа
-function popupOpen () {
-    popup.classList.add('popup_opened')
-}
-
-//Определяем функцию закрытия поп-апа
-function popupClose () {
-    popup.classList.remove('popup_opened')
-}
-
-//"Сулшаем" клик по кнопке редактирования профиля и открываем поп-ап при нажатии
-addBtn.addEventListener('click', popupOpen);
-
-//"Сулшаем" клик по кнопке закрытия поапа и закрываем поп-ап при нажатии
-closeBtn.addEventListener('click', popupClose);
+let editBtn = document.querySelector('.btn_type_edit');
+let popupCloseBtn = document.querySelector('.btn_type_close');
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__container');
@@ -29,6 +13,21 @@ let jobInput = formElement.querySelector('.input_type_about');
 //Находим поля блока Profile, куда нужно будет встаить новые значения
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
+
+//Определяем функцию открытия поп-апа
+function popupOpen () {
+    popup.classList.add('popup_opened');
+    
+    //Передаем значения провиля в поял формы
+    nameInput.textContent = profileName;
+    jobInput.textContent = profileAbout;
+
+}
+
+//Определяем функцию закрытия поп-апа
+function popupClose () {
+    popup.classList.remove('popup_opened');
+}
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -48,6 +47,13 @@ function formSubmitHandler (evt) {
     //закрываем поп-ап
     popupClose ();
 }
+
+
+//"Сулшаем" клик по кнопке редактирования профиля и открываем поп-ап при нажатии
+editBtn.addEventListener('click', popupOpen);
+
+//"Сулшаем" клик по кнопке закрытия поапа и закрываем поп-ап при нажатии
+popupCloseBtn.addEventListener('click', popupClose);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
