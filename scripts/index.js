0fdccc252af04add7cb6b1460e94cf178bd1fbd9
Mariya Stison <ladykleo@yandex.ru>
2022-06-22 09:02:19 +0300

    feat: реализовать добаление карточек по умолчанию через js

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,70 @@
+//Создаем 6 карточек "по умолчанию"
+//Определяем массиив с карточками
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+//Выбираем содержимое template
+const elementTemplate = document.querySelector('#elemnt').content;
+
+//Находим контейнер с карточками
+const elementsContainer = document.querySelector('.elemets');
+
+//Создаем карточки и заполняем содержимым
+for (i = 0; i < initialCards.length; i = i + 1) {
+  
+  //Клонируем содержимое тега template
+  const element = elementTemplate.querySelector('.elemnt').cloneNode(true);
+
+  //Заполняем содержимым
+  element.querySelector('.elemnt__img').src = initialCards[i].link;
+  element.querySelector('.elemnt__title').textContent = initialCards[i].name;
+  
+  //Отображаем на странице
+  elementsContainer.append(element);
+}
+
+
+
+
 //Находим поп-ап, кнопку Редактирования профиля, кнопку закрытия поп-апа
-let popup = document.querySelector('.popup');
-let editBtn = document.querySelector('.btn_type_edit');
-let popupCloseBtn = document.querySelector('.btn_type_close');
+const popup = document.querySelector('.popup');
+const editBtn = document.querySelector('.btn_type_edit');
+const popupCloseBtn = document.querySelector('.btn_type_close');
 
 // Находим форму в DOM
-let formElement = document.querySelector('.popup__container');
+const formElement = document.querySelector('.popup__container');
 
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.input_type_name');
-let jobInput = formElement.querySelector('.input_type_about');
+const nameInput = formElement.querySelector('.input_type_name');
+const jobInput = formElement.querySelector('.input_type_about');
 
 //Находим поля блока Profile, куда нужно будет встаить новые значения
-let profileName = document.querySelector('.profile__title');
-let profileAbout = document.querySelector('.profile__subtitle');
+const profileName = document.querySelector('.profile__title');
+const profileAbout = document.querySelector('.profile__subtitle');
 
 //Определяем функцию открытия поп-апа
 function popupOpen () {  
@@ -36,8 +88,8 @@ function formSubmitHandler (evt) {
                                                 // О том, как это делать, расскажем позже.
 
     // Получите значение полей jobInput и nameInput из свойства value
-    let nameNew = nameInput.value;
-    let aboutNew = jobInput.value;
+    const nameNew = nameInput.value;
+    const aboutNew = jobInput.value;
 
     // Вставьте новые значения с помощью textContent
     profileName.textContent = nameNew;
@@ -46,7 +98,6 @@ function formSubmitHandler (evt) {
     //закрываем поп-ап
     popupClose ();
 }
-
 
 //"Сулшаем" клик по кнопке редактирования профиля и открываем поп-ап при нажатии
 editBtn.addEventListener('click', popupOpen);
